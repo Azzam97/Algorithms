3931f400163b45563aba81daf0d2fb4e3978644d
Azzam Ahmad <azzam.ahmad97@gmail.com>
2023-07-08 11:21:24 +0300

    arrays to do assignment commit

---
+++ arrays_to_do_1.js
@@ -0,0 +1,76 @@
+//function to push to the front
+// function pushFront(arr,num){
+//     for (var x = arr.length-1; x > -1; x--) {
+//         arr[x+1] = arr[x]
+//     }
+//     arr[0] = num
+//     return arr
+// }
+// console.log(pushFront([5,7,2,3],8))
+
+//function to pop from the front
+// function popFront(arr){
+//     var value = arr[0]
+//     for (var x=0; x<arr.length; x++){
+//         arr[x] = arr[x+1]
+//     } 
+//     arr.pop([arr.length-1])
+//     console.log(value)
+//     return arr
+// }
+// console.log(popFront([4,5,7,9]))
+
+//a function that inserts at a certain index
+// function insertAt(arr,index,num){
+//     for (var x = arr.length-1; x>index-1; x--){
+//     arr[x+1] = arr[x]
+//     }
+//     arr[index] = num
+//     return arr
+// }
+// console.log(insertAt([100,200,5],2,311))
+
+//a function to remove an element in a set index
+// function removeAt(arr,index){
+//     var value = arr[index]
+//     for (var x = index; x < arr.length; x++){
+//         arr[x] = arr[x+1]
+//     }
+//     console.log(value)
+//     arr.pop()
+//     return arr
+// }
+// console.log(removeAt([1000,3,204,77],1))
+
+//a function to swap pairs and skipp the last element if it's odd
+// function swap(arr){
+//     if (arr.length/2==0){
+//         for (var x = 0; x < arr.length; x+=2){
+//             var value = arr[x]
+//             arr[x] = arr[x+1]
+//             arr[x+1] = value
+//         }
+//     }
+//     else{
+//         for (var x = 0; x < arr.length-1; x+=2){
+//             var value = arr[x]
+//             arr[x] = arr[x+1]
+//             arr[x+1] = value
+//         }
+//     }
+//     return arr
+// }
+// console.log(swap([1,2,3,4]))
+
+//a finction to remove dupes
+function removeDupes(arr){
+    for (var x = 0; x<arr.length; x++){
+        for (var y = arr.length-1; y>x; y--){
+            if (arr[x]==arr[y]){
+                arr.splice(y,1)
+            }
+        }
+    }
+    return arr
+}
+console.log(removeDupes([-2,-2,3.14,5,5,10]))
